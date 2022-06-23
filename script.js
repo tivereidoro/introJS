31a7b84123d475745a29633ad6b99f7c49a86760
Tivere IDORO <105525310+tivereidoro@users.noreply.github.com>
2022-06-23 04:03:20 +0100

    task completed

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const myName="Tivere Frederick IDORO";
 const height="6ft tall";
 const country="Nigeria";
 
-alert("\n NAME:" + myName + "\n\n" + "HEIGHT:" + height )
+alert("\n NAME: " + myName + " \n \n \n" + "HEIGHT: " + height + "\n \n \n" + "COUNTRY: " + country);
+    
+console.log(myName + " \n \n" + height +"\n \n " + country);
